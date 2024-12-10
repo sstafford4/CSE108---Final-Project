16ce96290c6de778b755f7d523b22c3eaaf41461
Spencer Stafford <spencestafford2187@gmail.com>
2024-12-09 17:23:32 -0800

    Users can create posts, topics, and can view posts from their feed.

--- back-end/main.py
+++ back-end/main.py
@@ -1,12 +1,132 @@
 from flask import render_template, request, url_for, redirect, flash, session
 from config import app, db
-from models import User
+from models import User, Topic, Posts
 
 app.secret_key = 'your_secret_key_here'  # Change this to a secure random key
+
+# @app.route('/')
+# def index():
+#     return render_template('login.html')
+#
+# # @app.route('/main_page')
+# # def main_page():
+# #     all_topics = Topic.query.all()
+# #     # for now, this will just get all of the posts in the db, however, I want to change it later so that it only gets posts
+# #     # relevant to the user (following or similar topics, etc)
+# #     relevant_posts = Posts.get.all()
+# #     return render_template('main_page.html', all_topics=all_topics, posts=relevant_posts)
+#
+# @app.route('/main_page')
+# def main_page():
+#     all_topics = Topic.query.all()
+#
+#     # This will get all posts in the database for now
+#     relevant_posts = Posts.query.all()  # This should be Posts.query.all(), not Posts.get.all()
+#
+#     return render_template('main_page.html', all_topics=all_topics, posts=relevant_posts)
+#
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         username = request.form.get('username')
+#         password = request.form.get('password')
+#
+#         user = User.query.filter_by(username=username).first()
+#         if user and user.check_password(password):
+#             session['logged_in'] = True
+#             session['user_id'] = user.id
+#             # this is going to be returning all topics so that they can be shown on the main hub page
+#             all_topics = Topic.query.all()
+#             relevant_posts = Posts.query.all()
+#             flash("You cool, mf")
+#             return render_template('main_page.html', all_topics=all_topics, user_id=user.id, username=user.username, posts=relevant_posts)
+#
+#         else:
+#             #flash("Invalid username or password")
+#             flash("Man, FUCK you.")
+#             return redirect(url_for('index'))
+#
+#     return redirect(url_for('index'))
+#
+# @app.route('/create_account', methods=['GET', 'POST'])
+# def create_account():
+#     if request.method == 'POST':
+#         real_name = request.form.get('real_name')
+#         username = request.form.get('username')
+#         email = request.form.get('email')
+#         password = request.form.get('password')
+#
+#         # Check if username already exists
+#         existing_user = User.query.filter_by(username=username).first()
+#         if existing_user:
+#             flash("This username is already taken.")
+#             return redirect(url_for('create_account'))
+#
+#         # Validate that password is not empty or None
+#         if not password:
+#             flash("Password cannot be empty.")
+#             return redirect(url_for('create_account'))
+#
+#         # Create a new user with a valid password
+#         new_user = User(real_name=real_name, username=username, email=email, password=password)  # Pass password directly here
+#         db.session.add(new_user)
+#         db.session.commit()
+#
+#         flash("Account created")
+#         return redirect(url_for('login'))
+#
+#     return render_template('login.html')
+#
+# @app.route('/create_post', methods=['GET', 'POST'])
+# def create_post():
+#     if request.method == 'POST':
+#         user_id = session.get('user_id')
+#         topic_name = request.form.get('post_topic')
+#         post_title = request.form.get('post_title')
+#         content = request.form.get('post_content')
+#
+#         # Check if the topic already exists
+#         existing_topic = Topic.query.filter_by(name=topic_name).first()
+#
+#         # If the topic doesn't exist, create it
+#         if not existing_topic:
+#             new_topic = Topic(name=topic_name, description=f"See what people are saying about {topic_name}!")
+#             db.session.add(new_topic)
+#             db.session.commit()  # Commit to get the new topic's ID
+#
+#             topic_id = new_topic.id  # Use the newly created topic's ID
+#         else:
+#             topic_id = existing_topic.id  # Use the existing topic's ID
+#
+#         # Create a new post with the appropriate topic_id
+#         new_post = Posts(poster_id=user_id, topic_id=topic_id, title=post_title, content=content)
+#         db.session.add(new_post)
+#         db.session.commit()
+#
+#         # Redirect or return a response, such as:
+#         return redirect(url_for('main_page'))  # Replace 'some_view' with the desired view after posting
+#
+#     # If it's a GET request, return the form to create a post
+#     return render_template('main_page.html')
 
 @app.route('/')
 def index():
     return render_template('login.html')
+
+@app.route('/main_page')
+def main_page():
+    # Ensure the user is logged in, and retrieve the username from session
+    if 'logged_in' in session:
+        user_id = session['user_id']
+        username = session.get('username')  # Retrieve the username from session
+
+        all_topics = Topic.query.all()
+        relevant_posts = Posts.query.all()
+
+        return render_template('main_page.html', all_topics=all_topics, posts=relevant_posts, username=username)
+    else:
+        flash("You need to log in first.")
+        return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -18,12 +138,15 @@ def login():
         if user and user.check_password(password):
             session['logged_in'] = True
             session['user_id'] = user.id
-            flash("You cool, mf")
-            return render_template('main_page.html')
+            session['username'] = user.username  # Store username in the session
 
+            all_topics = Topic.query.all()
+            relevant_posts = Posts.query.all()
+
+            flash("You cool, mf")
+            return render_template('main_page.html', all_topics=all_topics, user_id=user.id, username=user.username, posts=relevant_posts)
         else:
-            #flash("Invalid username or password")
-            flash("Man, FUCK you.")
+            flash("Invalid username or password")
             return redirect(url_for('index'))
 
     return redirect(url_for('index'))
@@ -31,6 +154,7 @@ def login():
 @app.route('/create_account', methods=['GET', 'POST'])
 def create_account():
     if request.method == 'POST':
+        real_name = request.form.get('real_name')
         username = request.form.get('username')
         email = request.form.get('email')
         password = request.form.get('password')
@@ -38,13 +162,16 @@ def create_account():
         # Check if username already exists
         existing_user = User.query.filter_by(username=username).first()
         if existing_user:
-            flash("This username is already taken bro. ")
+            flash("This username is already taken.")
             return redirect(url_for('create_account'))
 
-        # Create a new user and set hashed password
-        new_user = User(username=username, email=email, password=None)
-        new_user.set_password(password)
+        # Validate that password is not empty or None
+        if not password:
+            flash("Password cannot be empty.")
+            return redirect(url_for('create_account'))
 
+        # Create a new user with a valid password
+        new_user = User(real_name=real_name, username=username, email=email, password=password)  # Pass password directly here
         db.session.add(new_user)
         db.session.commit()
 
@@ -52,6 +179,40 @@ def create_account():
         return redirect(url_for('login'))
 
     return render_template('login.html')
+
+@app.route('/create_post', methods=['GET', 'POST'])
+def create_post():
+    if request.method == 'POST':
+        user_id = session.get('user_id')  # Get user_id from session
+        user = User.query.filter_by(id=user_id).first()
+        topic_name = request.form.get('post_topic')
+        post_title = request.form.get('post_title')
+        content = request.form.get('post_content')
+
+        # Check if the topic already exists
+        existing_topic = Topic.query.filter_by(name=topic_name).first()
+
+        # If the topic doesn't exist, create it
+        if not existing_topic:
+            new_topic = Topic(name=topic_name, description=f"See what people are saying about {topic_name}!")
+            db.session.add(new_topic)
+            db.session.commit()  # Commit to get the new topic's ID
+
+            topic_id = new_topic.id  # Use the newly created topic's ID
+        else:
+            topic_id = existing_topic.id  # Use the existing topic's ID
+
+        # Create a new post with the appropriate topic_id
+        new_post = Posts(poster_id=user_id, poster_username = user.username, topic_id=topic_id, title=post_title, content=content)
+        db.session.add(new_post)
+        db.session.commit()
+
+        # Redirect or return a response, such as:
+        return redirect(url_for('main_page'))  # Replace 'some_view' with the desired view after posting
+
+    # If it's a GET request, return the form to create a post
+    return render_template('main_page.html')
+
 
 
 if __name__ == '__main__':
--- back-end/models.py
+++ back-end/models.py
@@ -1,25 +1,153 @@
+# from config import db
+# from werkzeug.security import generate_password_hash, check_password_hash
+# from datetime import datetime
+#
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     real_name = db.Column(db.String(64), index=True, unique=True)
+#     username = db.Column(db.String(100), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(250), nullable=False)
+#
+#     def __init__(self, real_name, username, email, password):
+#         self.real_name = real_name
+#         self.username = username
+#         self.email = email
+#
+#     # werkzeug auto-salts all of the passwords it hashes, so we dont need any further password adjustments.
+#     def set_password(self, password):
+#         self.password = generate_password_hash(password)
+#
+#     def check_password(self, password):
+#         return check_password_hash(self.password, password)
+#
+#     def to_json(self):
+#         return {
+#             'realName': self.real_name,
+#             'username': self.username,
+#             'email': self.email
+#         }
+#
+# class Topic(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#
+#
+# class Posts(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     poster_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
+#     title = db.Column(db.String(64), index=True, unique=True, nullable=False)
+#     content = db.Column(db.Text, nullable=False)
+#     created_at = db.Column(db.DateTime, default=datetime.now)
+#     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+#
+#     def __init__(self, poster_id, title, content):
+#         self.poster_id = poster_id
+#         self.title = title
+#         self.content = content
+#
+#     def to_json(self):
+#         return {
+#             'id': self.id,
+#             'poster_id': self.poster_id,
+#             'title': self.title,
+#             'content': self.content,
+#             'created_at': self.created_at.isoformat(),
+#             'updated_at': self.updated_at.isoformat()
+#         }
+
+
 from config import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+# Association table for Users following Topics
+user_topic = db.Table(
+    'user_topic',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('topic_id', db.Integer, db.ForeignKey('topic.id'), primary_key=True)
+)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    real_name = db.Column(db.String(64), index=True, unique=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(250), nullable=False)
 
-    def __init__(self, username, email, password):
+    # Relationship to the posts created by the user
+    posts = db.relationship('Posts', backref='poster', lazy='dynamic')
+
+    # Many-to-many relationship for followed topics
+    followed_topics = db.relationship('Topic', secondary=user_topic, backref=db.backref('followers', lazy='dynamic'))
+
+    def __init__(self, real_name, username, email, password):
+        self.real_name = real_name
         self.username = username
         self.email = email
+        self.set_password(password)  # Pass the password here to ensure it's set correctly
 
-    # werkzeug auto-salts all of the passwords it hashes, so we dont need any further password adjustments.
     def set_password(self, password):
-        self.password = generate_password_hash(password)
+        if password:  # Make sure the password is not empty or None
+            self.password = generate_password_hash(password)
+        else:
+            raise ValueError("Password cannot be empty.")  # Optional: Add custom validation
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
     def to_json(self):
         return {
+            'realName': self.real_name,
             'username': self.username,
             'email': self.email
+        }
+
+class Topic(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False, index=True)
+    description = db.Column(db.Text, nullable=True)
+
+    # Relationship to the posts in this topic
+    posts = db.relationship('Posts', backref='topic', lazy='dynamic')
+
+    def __init__(self, name, description=None):
+        self.name = name
+        self.description = description
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'post_count': self.posts.count(),
+            'followers_count': self.followers.count()
+        }
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    poster_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
+    poster_username = db.Column(db.String(100), unique=True, nullable=False)
+    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'), index=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+    def __init__(self, poster_id, poster_username, topic_id, title, content):
+        self.poster_id = poster_id
+        self.poster_username = poster_username
+        self.topic_id = topic_id
+        self.title = title
+        self.content = content
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'poster_id': self.poster_id,
+            'poster_username': self.poster_username,
+            'topic_id': self.topic_id,
+            'title': self.title,
+            'content': self.content,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat()
         }
